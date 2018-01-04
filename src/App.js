c94132ab816c4efbc217a8f86d611241b00179ea
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-01-04 17:14:54 -0500

    Made small changes in Search.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Search from './Components/Search';
 const API_KEY = 'AIzaSyD8JXAk7tF4c4DJos6E7jCn_rAyQ0xnUIM';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-Youtuber
+        <Search  />
       </div>
     );
   }
