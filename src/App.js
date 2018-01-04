098a2d06533bce76333264d0529f00f06bd7a759
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-01-04 17:50:50 -0500

    Input state is working  in the search.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Search from './Components/Search';
-const API_KEY = 'AIzaSyD8JXAk7tF4c4DJos6E7jCn_rAyQ0xnUIM';
+//const API_KEY = 'AIzaSyD8JXAk7tF4c4DJos6E7jCn_rAyQ0xnUIM';
 
 class App extends Component {
   render() {
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ''
+        };       
+    }
+
+
+
     render() {
+
         return (
             <div>
-                From Search
+                <input type="text" placeholder="Search" 
+                    onChange={e => this.setState({input: e.target.value})}
+                />
+
 
             </div>
         )
