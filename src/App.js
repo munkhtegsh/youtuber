11a7dbdcabf4a4e59c42b5691d7573f337a80472
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-01-04 21:09:44 -0500

    Moved YoutubeSearch to the constractor in App.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import YoutubeSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCP9i60WSl8NaprDSmTW8CZjiUb0KnhkDA';
 
 
-YoutubeSearch({key: API_KEY, term: 'surfBoards'}, (data) => {
-  console.log(data);
-});
-
-
 class App extends Component {
 
   constructor(props) {
     super();
     this.state = {
-      term: ''
+      videos: []
     }
+
+    YoutubeSearch({key: API_KEY, term: 'surfBoards'}, (data) => {
+      this.setState({videos: data});
+    });
   }
 
 
-  
 
   render() {
     return (
